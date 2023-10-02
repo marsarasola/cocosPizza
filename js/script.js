1e2c48e6caf2670dea4ce932d58a0dc4b8c1af47
marsarasola <sarasolamartina21@gmail.com>
2023-10-02 17:23:49 -0300

    .

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY > 0);
 })
+
+
+const navIcon = document.querySelector('.show-menu')
+const navList = document.querySelector('#menu')
+
+navIcon.onclick = () => navList.classList.toggle('open')
