d63141b7b5298e1a688b0dd06082346437dc280d
marsarasola <sarasolamartina21@gmail.com>
2023-10-04 12:00:09 -0300

    footer

--- js/script.js
+++ js/script.js
@@ -5,7 +5,16 @@ window.addEventListener("scroll", function () {
 })
 
 
-const navIcon = document.querySelector('.show-menu')
-const navList = document.querySelector('#menu')
+// const navIcon = document.querySelector('.show-menu')
+// const navList = document.querySelector('#menu')
 
-navIcon.onclick = () => navList.classList.toggle('open')
+// navIcon.onclick = () => navList.classList.toggle('open')
+
+
+const menuToggle = document.getElementById('show-menu');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  // Toggle para mostrar/ocultar el menú
+  menu.style.transform = menu.style.transform === 'translateX(0)' ? 'translateX(100%)' : 'translateX(0)';
+});
