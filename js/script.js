66b89df006d4f02ea200b6cebe78fc83f3bc073c
lautroRocha <lautarovrocha@gmail.com>
2023-10-17 19:36:45 -0300

    feat: hanlde menu with JS

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("abajo", window.scrollY > 0);
 })
 
+const sideMenu = document.querySelector('#menu')
+const menuIcon = document.querySelector('#show-menu')
 
+document.querySelectorAll('#navLink').forEach( (node) => {
+    node.addEventListener('click', () => {
+        sideMenu.classList.remove('open')
+    })
+})
 
+menuIcon.onclick = () => sideMenu.classList.toggle('open')
 
 
