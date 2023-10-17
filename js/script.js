e650f87526f8937b6e7d081a9b7cf980f858de62
lautroRocha <lautarovrocha@gmail.com>
2023-10-17 19:41:07 -0300

    docs

--- js/script.js
+++ js/script.js
@@ -4,15 +4,17 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("abajo", window.scrollY > 0);
 })
 
+//selecciono el menu y el ícono hamburguesa
 const sideMenu = document.querySelector('#menu')
 const menuIcon = document.querySelector('#show-menu')
 
+//selecciono todos los link de la nav, y por cada uno tomo el nodo y le agrego un listener
 document.querySelectorAll('#navLink').forEach( (node) => {
     node.addEventListener('click', () => {
         sideMenu.classList.remove('open')
     })
 })
-
+//al clickear el botón, toglea (quito y pongo) la clase open
 menuIcon.onclick = () => sideMenu.classList.toggle('open')
 
 
