294e174a2c799eb4d357500b9d21d3609566358d
elijt1994 <joshpleing@gmail.com>
2024-01-28 20:51:12 -0300

    Update 28/01/24

--- js/script.js
+++ js/script.js
@@ -2,16 +2,21 @@
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY > 0);
-})
+});
 
-const sideMenu = document.querySelector('#menu')
-const menuIcon = document.querySelector('#show-menu')
+const sideMenu = document.querySelector('#menu');
+const menuIcon = document.querySelector('#show-menu');
 
-document.querySelectorAll('#navLink').forEach( (node) => {
+menuIcon.addEventListener('change', () => {
+    sideMenu.classList.toggle('open', menuIcon.checked);
+});
+
+document.querySelectorAll('.navLink').forEach((node) => {
     node.addEventListener('click', () => {
-        sideMenu.classList.remove('open')
-    })
-})
+        sideMenu.classList.remove('open');
+        menuIcon.checked = false;
+    });
+});
 
 /***** Video Presentation - Button Sound *****/
 document.addEventListener("DOMContentLoaded", function () {
@@ -22,14 +27,5 @@ document.addEventListener("DOMContentLoaded", function () {
         toggleMute();
     });
 
-    function toggleMute() {
-        video.muted = !video.muted;
-
-        if (video.muted) {
-            muteButton.src = "./assets/icons/sound-off.png";
-        } else {
-            muteButton.src = "./assets/icons/sound-on.png";
-        }
-    }
 });
 
