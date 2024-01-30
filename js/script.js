65787f9aa4635785f414a30880fa4953676aa167
elijt1994 <joshpleing@gmail.com>
2024-01-30 02:44:34 -0300

    Last update (the finish proyect 9)

--- js/script.js
+++ js/script.js
@@ -1,21 +1,57 @@
-/***** NavBar *****/
+/***** NavBar Transition *****/
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY > 0);
 });
 
-const sideMenu = document.querySelector('#menu');
-const menuIcon = document.querySelector('#show-menu');
+/***** NavBar Responsive *****/
+const sideMenu = document.getElementById("menu");
+const menuIcon = document.getElementById("show-menu");
 
-menuIcon.addEventListener('change', () => {
-    sideMenu.classList.toggle('open', menuIcon.checked);
+function showMenu() {
+    if (window.innerWidth <= 767) {
+        sideMenu.classList.add('open');
+        sideMenu.style.display = 'flex';
+    }
+}
+
+function hideMenu() {
+    if (window.innerWidth <= 767) {
+        sideMenu.classList.remove('open');
+        sideMenu.style.display = 'none';
+    }
+}
+
+function toggleMenu() {
+    if (window.innerWidth <= 767) {
+        sideMenu.classList.toggle('open');
+        if (sideMenu.classList.contains('open')) {
+            sideMenu.style.display = 'flex';
+        } else {
+            hideMenu();
+        }
+    } else {
+        sideMenu.classList.add('open');
+        sideMenu.style.display = 'flex';
+    }
+}
+
+menuIcon.addEventListener('click', toggleMenu);
+sideMenu.addEventListener('mouseenter', showMenu);
+sideMenu.addEventListener('mouseleave', hideMenu);
+
+const menuLinks = document.querySelectorAll('#menu a.navLink');
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        hideMenu();
+    });
 });
 
-document.querySelectorAll('.navLink').forEach((node) => {
-    node.addEventListener('click', () => {
-        sideMenu.classList.remove('open');
-        menuIcon.checked = false;
-    });
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 767) {
+        sideMenu.classList.add('open');
+        sideMenu.style.display = 'flex';
+    }
 });
 
 /***** Video Presentation - Button Sound *****/
